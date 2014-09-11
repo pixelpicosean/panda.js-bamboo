b71159bbf3de7215b585439121307a9cd750e318
Eemeli Kelokorpi <eemeli.kelokorpi@yle.fi>
2014-09-11 21:13:02 +0300

    Update

--- src/bamboo/runtime/nodes/layer.js
+++ src/bamboo/runtime/nodes/layer.js
@@ -11,7 +11,31 @@ game.module(
 bamboo.createNode('Layer', {
     ready: function() {
         if (this.cacheAsBitmap) {
-            this.displayObject.cacheAsBitmap = true;
+            var size = 2048;
+            if (this.displayObject.width > size || this.displayObject.height > size) {
+                var tx = Math.ceil(this.displayObject.width / size);
+                var ty = Math.ceil(this.displayObject.height / size);
+                var pos = new game.Point();
+                var renderTexture;
+                var sprites = [];
+                var sprite;
+                for (var y = 0; y < ty; y++) {
+                    for (var x = 0; x < tx; x++) {
+                        pos.set(-x * size, -y * size);
+                        renderTexture = new game.RenderTexture(size, size);
+                        renderTexture.render(this.displayObject, pos);
+                        sprite = new game.Sprite(renderTexture);
+                        sprite.position.set(x * size, y * size);
+                        sprites.push(sprite);
+                    }
+                }
+                for (var i = this.displayObject.children.length - 1; i >= 0; i--) {
+                    this.displayObject.removeChild(this.displayObject.children[i]);
+                }
+                for (var i = 0; i < sprites.length; i++) {
+                    this.displayObject.addChild(sprites[i]);
+                }
+            }
         }
     }
 });
