e03bbf8dc069a9c8402724bc2507c553bf600245
Eemeli Kelokorpi <eemeli.kelokorpi@yle.fi>
2014-10-08 15:23:31 +0300

    Fixed edit mode state

--- src/bamboo/editor/modes/edit.js
+++ src/bamboo/editor/modes/edit.js
@@ -10,42 +10,42 @@ bamboo.editor.ModeEdit = bamboo.editor.Mode.extend({
     helpText: 'Edit mode: MOUSE confirm, ESC cancel',
 
     enter: function() {
-        this.state = this.editor.activeNode._editorNode;
-        this.editor.controller.enableEditMode(this.state, true);
+        this.node = this.editor.activeNode._editorNode;
+        this.editor.controller.enableEditMode(this.node, true);
     },
     
     exit: function() {
-        this.editor.controller.enableEditMode(this.state, false);
+        this.editor.controller.enableEditMode(this.node, false);
     },
 
     click: function(event) {
         var pos = new game.Point(event.global.x, event.global.y);
         pos = this.editor.toWorldSpace(pos);
-        this.state.click(pos);
+        this.node.click(pos);
     },
 
     mousedown: function(event) {
         var pos = new game.Point(event.global.x, event.global.y);
         pos = this.editor.toWorldSpace(pos);
-        this.state.mousedown(pos);
+        this.node.mousedown(pos);
     },
 
     mousemove: function(event) {
         var pos = new game.Point(event.global.x, event.global.y);
         pos = this.editor.toWorldSpace(pos);
-        this.state.mousemove(pos);
+        this.node.mousemove(pos);
     },
 
     mouseup: function(event) {
         var pos = new game.Point(event.global.x, event.global.y);
         pos = this.editor.toWorldSpace(pos);
-        this.state.mouseup(pos);
+        this.node.mouseup(pos);
     },
 
     keydown: function(key) {
         if (key === 'ESC') return this.editor.changeMode('Main');
         
-        this.state.keydown(key);
+        this.node.keydown(key);
     }
 });
 
