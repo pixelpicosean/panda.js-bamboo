01f0a2478101237fce8420bb49d8eec6df505703
Eemeli Kelokorpi <eemeli.kelokorpi@yle.fi>
2014-10-09 13:15:53 +0300

    Added MouseTrigger

---
+++ src/bamboo/runtime/nodes/mousetrigger.js
@@ -0,0 +1,38 @@
+game.module(
+    'bamboo.runtime.nodes.mousetrigger'
+)
+.require(
+    'bamboo.core',
+    'bamboo.runtime.node'
+)
+.body(function() {
+
+/**
+    Calls children's trigger function, when trigger is clicked.
+    @class Trigger
+    @namespace bamboo.Nodes
+**/
+bamboo.createNode('MouseTrigger', {
+    ready: function() {
+        this.displayObject.interactive = true;
+        this.displayObject.buttonMode = !!this.buttonMode;
+        this.displayObject.click = this.displayObject.tap = this.click.bind(this);
+        this.displayObject.hitArea = new game.HitRectangle(0, 0, this.size.x, this.size.y);
+    },
+
+    click: function() {
+        for (var i = 0; i < this.children.length; i++) {
+            this.children[i].trigger(this);
+        }
+        if (this.onetime) this.displayObject.interactive = false;
+    }
+});
+
+/**
+    Disable trigger, after clicked.
+    @property {Boolean} onetime
+**/
+bamboo.addNodeProperty('Trigger', 'onetime', 'boolean');
+bamboo.addNodeProperty('Trigger', 'buttonMode', 'boolean');
+
+});
