e997091e632c033f59fd1d236aa0b44ba0935f3f
Eemeli Kelokorpi <eemeli.kelokorpi@yle.fi>
2014-10-10 12:38:56 +0300

    Added timestamp to console

--- src/bamboo/editor/console.js
+++ src/bamboo/editor/console.js
@@ -17,8 +17,21 @@ bamboo.Console = game.Class.extend({
     },
 
     log: function(message) {
-        this.messages.unshift(message);
+        this.messages.unshift(this.getTime() + ' - ' + message);
         this.update();
+    },
+
+    getTime: function() {
+        var currentTime = new Date();
+        var hours = currentTime.getHours();
+        var minutes = currentTime.getMinutes();
+        var seconds = currentTime.getSeconds();
+
+        if (hours === 0) hours = '0' + hours;
+        if (minutes < 10) minutes = '0' + minutes;
+        if (seconds < 10) seconds = '0' + seconds;
+
+        return hours + ':' + minutes + ':' + seconds;
     },
 
     update: function() {
